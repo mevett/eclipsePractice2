6400a0443e0371ec786ec5f0bef3f90db91e696e
mevett <mevett@emich.edu>
2023-09-27 17:32:10 -0400

    tweaked Driver to see if push still works to github

--- src/eclipseProject/Driver.java
+++ src/eclipseProject/Driver.java
@@ -3,7 +3,7 @@ package eclipseProject;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("my driver works");
+		System.out.println("my driver really works");
 		System.out.println("altered on PC");
 
 	}
